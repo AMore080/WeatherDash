ce75245a9d69b73689da8db366bea16147c29526
Alex <alexmore080@gmail.com>
2022-08-12 12:30:22 -0500

    user input will now display info through the handleWeatherInformation

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
 let searchForm = document.querySelector("#form-el");
-let temperature = document.getAttribute("temp")
 
+
+fetch 
 
 //Handle the input from the User
 function handleSearchFormSubmit(event){
     event.preventDefault();
 
-    let searchInput = document.querySelector("#search").value;
-    let cityInput = document.querySelector("#city").value;
+    let searchInput = document.querySelector("#search").value.trim();
+    let cityInput = document.querySelector("#city").value.trim();
 
     if(!searchInput){
         console.error("Please enter a city.")
@@ -16,7 +17,20 @@ function handleSearchFormSubmit(event){
 
     console.log(searchInput)
     console.log(cityInput)
+    handleWeatherInformation(cityInput);
 }
 
 searchForm.addEventListener('submit',handleSearchFormSubmit);
 
+function handleWeatherInformation(cityInput){
+    let apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=c6fb8cbc7651b34c42f309d531651a96"
+
+    fetch(apiURL).then(function (response){
+        if(response.ok){
+            response.json().then(function (data){
+                console.log(data)
+            })
+        }
+    })
+    displayWeatherBlocks(data)
+}
